4804db1b72b1b383cd16ce82203f79428aa6fd4e
vy822004 <vy822004@gmail.com>
2025-09-14 20:14:49 +0530

    Add files via upload

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,12 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/Layout";
 import Dashboard from "./pages/Dashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 import Appointments from "./pages/Appointments";
 import Messages from "./pages/Messages";
 import Forum from "./pages/Forum";
 import Resources from "./pages/Resources";
 import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
+import PrivateRoute from "./pages/PrivateRoute";
 
 const queryClient = new QueryClient();
 
@@ -28,7 +30,18 @@ const App = () => (
             <Route path="/forum" element={<Forum />} />
             <Route path="/resources" element={<Resources />} />
             <Route path="/settings" element={<Settings />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+            {/* Admin Dashboard - Protected */}
+            <Route 
+              path="/admin" 
+              element={
+                <PrivateRoute role="admin">
+                  <AdminDashboard />
+                </PrivateRoute>
+              } 
+            />
+
+            {/* Catch-all 404 route */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
