731a2aea600ccdd8cfd70d465c2a550356748e09
Piyush Kumar <piyushk9321@gmail.com>
2025-09-15 15:59:04 +0530

    Delete Frontend directory

--- Frontend/src/pages/AdminDashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { Card } from "@/components/ui/card";
-import { Users, Activity, BarChart3 } from "lucide-react";
-
-export default function AdminDashboard() {
-  return (
-   <p>asdsa</p>
-  );
-}
